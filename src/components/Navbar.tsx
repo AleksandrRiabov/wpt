2da024c2383aafafc046b955b99984bff8e4a179
Aleksandr Riabov <sania.riabov@gmail.com>
2023-02-28 23:48:02 +0000

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Box, Button, Typography } from '@mui/material';
+
+const Navbar = () => {
+  return (
+    <Box
+
+    >
+      <Typography
+        sx={{
+          color: 'primary.main'
+        }}>NavbarOut</Typography>
+      <Typography
+        sx={{
+          color: 'primary.light'
+        }}>NavbarOut</Typography>
+      <Typography
+        sx={{
+          color: 'secondary.main'
+        }}>NavbarOut</Typography>
+      <Typography
+        sx={{
+          color: 'secondary.light'
+        }}>NavbarOut</Typography>
+      <Button variant='contained' color="secondary">Navbar</Button>
+    </Box>
+  )
+}
+
+export default Navbar
