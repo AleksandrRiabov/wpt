0d3ed9fd4cd7fe73c448e0a7da66aa4407206123
Aleksandr Riabov <sania.riabov@gmail.com>
2023-04-18 13:08:03 +0100

    Small refactoring, unused code removed

--- src/hooks/useFormatBarChartData.ts
+++ src/hooks/useFormatBarChartData.ts
@@ -14,27 +14,26 @@ const useFormatBarChartData = ({ data, checkedProducts }: Props) => {
 
     let totalsForThePeriod: Totals = {};
 
-    data &&
-      data?.forEach(({ products }) => {
-        products.forEach((product) => {
-          if (
-            checkedProducts.includes(product.name) ||
-            checkedProducts.includes(product.category)
-          ) {
-            if (!totalsForThePeriod[product.name]) {
-              totalsForThePeriod[product.name] = { cases: 0, pallets: 0 };
-            }
-
-            totalsForThePeriod[product.name] = {
-              cases:
-                totalsForThePeriod[product.name].cases + Number(product.cases),
-              pallets:
-                totalsForThePeriod[product.name].pallets +
-                Number(product.pallets),
-            };
+    data?.forEach(({ products }) => {
+      products.forEach((product) => {
+        if (
+          checkedProducts.includes(product.name) ||
+          checkedProducts.includes(product.category)
+        ) {
+          if (!totalsForThePeriod[product.name]) {
+            totalsForThePeriod[product.name] = { cases: 0, pallets: 0 };
           }
-        });
+
+          totalsForThePeriod[product.name] = {
+            cases:
+              totalsForThePeriod[product.name].cases + Number(product.cases),
+            pallets:
+              totalsForThePeriod[product.name].pallets +
+              Number(product.pallets),
+          };
+        }
       });
+    });
 
     return Object.keys(totalsForThePeriod).map((product) => {
       const { cases, pallets } = totalsForThePeriod[product];
