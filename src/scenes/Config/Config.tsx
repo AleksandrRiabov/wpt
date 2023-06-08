68e0ff467249c29dffd08e0c9932c8472e11bce8
Aleksandr Riabov <sania.riabov@gmail.com>
2023-06-08 23:04:16 +0100

    Display page header regardless error or loading

--- src/scenes/Config/Config.tsx
+++ src/scenes/Config/Config.tsx
@@ -19,67 +19,63 @@ const Config = () => {
     setUpdatedDataState(data);
   }, [data]);
 
-  if (isError)
-    return (
-      <FlexCenterCenter height="90vh">
-        <Typography variant="h4">
-          "Error.. Could not get options. Please Try again later.."
-        </Typography>
-      </FlexCenterCenter>
-    );
-
-  if (isLoading)
-    return (
-      <FlexCenterCenter height="90vh">
-        <CircularProgress />
-      </FlexCenterCenter>
-    );
-
   console.log("render");
 
   return (
     <Container maxWidth="xl" sx={{ marginBottom: "20px" }}>
       <PageHeader title="Edit Configurations" />
-      <DashboardBox sx={{ padding: { xs: "20px 5px", sm: "20px" } }}>
-        <Box
-          display="flex"
-          sx={{
-            flexDirection: { xs: "column", md: "row" },
-            flexWrap: "wrap",
-            justifyContent: "center",
-            gap: "2rem",
-            padding: { md: "20px 0" },
-            margin: "0 auto",
-          }}
-        >
-          {/* Freight Type */}
-          <ConfigBox
-            title="Freight-Type"
-            name="freightType"
-            configCategory={updatedDataState?.freightType || []}
-          />
-          {/* Contractor */}
-          <ConfigBox
-            title="Contractors"
-            name="contractor"
-            configCategory={updatedDataState?.contractor || []}
-          />
-          {/* Crossed */}
-          <ConfigBox
-            title="Crossed"
-            name="crossed"
-            configCategory={updatedDataState?.crossed || []}
-          />
-          {/* Load Type */}
-          <ConfigBox
-            title="Load-Type"
-            name="loadType"
-            configCategory={updatedDataState?.loadType || []}
-          />
-          {/* Products */}
-          <ConfigProductBox products={updatedDataState?.products || []} />
-        </Box>
-      </DashboardBox>
+      {isLoading ? (
+        <FlexCenterCenter height="90vh">
+          <CircularProgress />
+        </FlexCenterCenter>
+      ) : isError ? (
+        <FlexCenterCenter height="90vh">
+          <Typography variant="h4">
+            "Error.. Could not get options. Please Try again later.."
+          </Typography>
+        </FlexCenterCenter>
+      ) : (
+        <DashboardBox sx={{ padding: { xs: "20px 5px", sm: "20px" } }}>
+          <Box
+            display="flex"
+            sx={{
+              flexDirection: { xs: "column", md: "row" },
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "2rem",
+              padding: { md: "20px 0" },
+              margin: "0 auto",
+            }}
+          >
+            {/* Freight Type */}
+            <ConfigBox
+              title="Freight-Type"
+              name="freightType"
+              configCategory={updatedDataState?.freightType || []}
+            />
+            {/* Contractor */}
+            <ConfigBox
+              title="Contractors"
+              name="contractor"
+              configCategory={updatedDataState?.contractor || []}
+            />
+            {/* Crossed */}
+            <ConfigBox
+              title="Crossed"
+              name="crossed"
+              configCategory={updatedDataState?.crossed || []}
+            />
+            {/* Load Type */}
+            <ConfigBox
+              title="Load-Type"
+              name="loadType"
+              configCategory={updatedDataState?.loadType || []}
+            />
+            {/* Products */}
+            <ConfigProductBox products={updatedDataState?.products || []} />
+          </Box>
+        </DashboardBox>
+      )}
     </Container>
   );
 };
