6356bb3ded64e677ef3ec2a404b3e241922ae1af
Aleksandr Riabov <sania.riabov@gmail.com>
2023-09-08 16:23:13 +0100

    Guest Mode button added, free hosting notice alert added

--- src/scenes/auth/Login/Login.tsx
+++ src/scenes/auth/Login/Login.tsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import { Container, TextField, Button, Box, Typography } from "@mui/material";
+import {
+  Container,
+  TextField,
+  Button,
+  Box,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import LoginWithGoogle from "./LoginWithGoogle";
 import { UserAuth } from "../../../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
@@ -8,6 +15,10 @@ import DashboardBox from "../../../components/dashboardBox/DashboardBox";
 import FlexBetween from "../../../components/FlexBetween/FlexBetween";
 import useSendAnalytics from "../../../hooks/useSendAnalytics";
 import "../style.css";
+import { tokens } from "../../../theme";
+
+const notice =
+  "Because of free hosting, initial loading may take a while. Thank you for your patience.";
 
 const Login = () => {
   useSendAnalytics({ title: "Login Page" });
@@ -16,13 +27,19 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
+  const [invalidEmail, setInvalidEmail] = useState("");
+  const [invalidPassword, setInvalidPassword] = useState("");
 
   const navigate = useNavigate();
 
   const { signInUser, user } = UserAuth();
+  // Get theme and colors from MUI theme
+  const { palette } = useTheme();
+  const colors = tokens(palette.mode);
 
   useEffect(() => {
     if (user) {
+      alert(notice);
       navigate("/dashboard");
     }
   }, [user, navigate]);
@@ -30,6 +47,17 @@ const Login = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setErrorMessage("");
+
+    if (!email.trim().length) {
+      return setInvalidEmail("Please enter your email");
+    } else {
+      setInvalidEmail("");
+    }
+    if (!password.trim().length) {
+      return setInvalidPassword("Please enter your password");
+    } else {
+      setInvalidPassword("");
+    }
 
     try {
       await signInUser(email, password);
@@ -45,6 +73,16 @@ const Login = () => {
       setErrorMessage("");
     } else {
       setSuccessMessage("");
+    }
+  };
+
+  const handleGuestModelogin = async () => {
+    try {
+      await signInUser("visitor@wpt.com", "1234567Wpt");
+      setSuccessMessage(`Successfully ${user?.displayName} Signed in`);
+    } catch (error: any) {
+      console.log(error);
+      setErrorMessage(error.message);
     }
   };
 
@@ -87,6 +125,8 @@ const Login = () => {
                 sx={{
                   margin: "10px 0",
                 }}
+                error={invalidEmail.length > 0}
+                helperText={invalidEmail}
               />
               <TextField
                 label="Password"
@@ -95,10 +135,25 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 fullWidth
                 color="secondary"
+                error={invalidPassword.length > 0}
+                helperText={invalidPassword}
               />
               <FlexBetween p="30px 0">
                 <Button type="submit" variant="contained" color="secondary">
                   Login
+                </Button>
+                <Button
+                  variant="contained"
+                  sx={{
+                    color: colors.secondary[400],
+                    background: colors.lightBlue[600],
+                    "&:hover": {
+                      background: colors.lightBlue[700],
+                    },
+                  }}
+                  onClick={handleGuestModelogin}
+                >
+                  Guest Mode
                 </Button>
               </FlexBetween>
             </form>
